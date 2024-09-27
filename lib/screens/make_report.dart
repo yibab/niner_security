a9f49690ee202f94a2381b60e8be061f9ec20c3f
yibab <andreybabiy01@yahoo.com>
2024-09-27 16:14:52 -0400

    redid the home page

--- lib/screens/make_report.dart
+++ lib/screens/make_report.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:niner_security/widgets/footer.dart';
 import 'package:niner_security/widgets/niner_text.dart';
 import 'package:niner_security/widgets/report_type.dart';
 
 class MakeReport extends StatelessWidget {
-  const MakeReport({super.key});
+  final String reportType;
+
+  const MakeReport({super.key, required this.reportType});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: NinerText(),
+        title: const NinerText(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: [
-              ReportType()],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Column(
+                children: [
+                  ReportType(
+                    reportType: reportType,
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+          const Spacer(),
+          const Copyright(),
+        ],
       ),
     );
   }
