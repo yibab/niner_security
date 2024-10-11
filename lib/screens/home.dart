08f021eea2d2db51360a7d4c208bc87b7693f39c
yibab <andreybabiy01@yahoo.com>
2024-10-11 19:55:07 -0400

    git example for hw

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,7 +17,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Column(
