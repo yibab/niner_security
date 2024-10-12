810150a6e5978bde1e2ed3c38c2c8065d341ef99
Austin Sears <snowyparadox91@gmail.com>
2024-10-11 23:42:32 -0400

    Test for GIT video

--- lib/widgets/signin_button.dart
+++ lib/widgets/signin_button.dart
@@ -24,7 +24,7 @@ class _SigninButtonState extends State<SigninButton> {
             borderRadius: BorderRadius.circular(8)),
         child: const Center(
           child: Text(
-            "Sign Up",
+            "Sign In",
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
